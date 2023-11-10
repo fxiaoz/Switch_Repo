08ba930a84ace866970043b9bbbb2e09b5cbe001
Xiao <sz3456@nyu.edu>
2023-11-10 11:44:12 -0500

    room 2 objects

--- Switch/Assets/InputHandler.cs
+++ Switch/Assets/InputHandler.cs
@@ -17,23 +17,35 @@ public class InputHandler : MonoBehaviour
     public string[] dialogue;
     private int index;
     public float wordspeed;
+    List<string> inventory = new List<string>();
 
     public GameObject continueButton;
     public GameObject closeButton;
+
+ 
+    string sceneName;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
 
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+
+       
+        
         dialogueBox.SetActive(false);
         text.SetActive(false);
         closeButton.SetActive(false);
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetMouseButtonDown(0))
         {
             if (dialogueBox.activeInHierarchy)
@@ -43,16 +55,27 @@ public class InputHandler : MonoBehaviour
 
             else
             {
-                dialogueBox.SetActive(true);
-                text.SetActive(true);
-                StartCoroutine(Typing());
+                
+            }
+        }
+        */
+
+        if (Input.GetKeyDown("space"))
+        {
+            foreach (var x in inventory)
+            {
+                Debug.Log(x);
             }
         }
 
+        
+        
         if (Text.text == dialogue[index])
         {
             continueButton.SetActive(true);
         }
+        
+        
     }
 
     public void OnClick(InputAction.CallbackContext context)
@@ -60,7 +83,19 @@ public class InputHandler : MonoBehaviour
         if (!context.started) return;
 
         var rayHit = Physics2D.GetRayIntersection(mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
-        if (!rayHit.collider) return;
+        if (!rayHit.collider)
+        {
+            return;
+        }
+        else
+        {
+            
+            
+            dialogueBox.SetActive(true);
+            text.SetActive(true);
+            StartCoroutine(Typing());
+            
+        }
 
         if (rayHit.collider.gameObject.name == "knife")
         {
@@ -68,6 +103,11 @@ public class InputHandler : MonoBehaviour
             text.SetActive(true);
             closeButton.SetActive(true);
             Text.text = "Has a right-handed bloody handprint on the handle.";
+
+            if (!inventory.Contains(rayHit.collider.gameObject.name))
+            {
+                inventory.Add(rayHit.collider.gameObject.name);
+            }
         }
 
         if (rayHit.collider.gameObject.name == "rope")
@@ -76,6 +116,11 @@ public class InputHandler : MonoBehaviour
             text.SetActive(true);
             closeButton.SetActive(true);
             Text.text = "Looks like it was cut with something sharp.";
+
+            if (!inventory.Contains(rayHit.collider.gameObject.name))
+            {
+                inventory.Add(rayHit.collider.gameObject.name);
+            }
         }
 
         if (rayHit.collider.gameObject.name == "mug")
@@ -84,28 +129,79 @@ public class InputHandler : MonoBehaviour
             text.SetActive(true);
             closeButton.SetActive(true);
             Text.text = "An empty mug with traces of coffee.";
+
+            if (!inventory.Contains(rayHit.collider.gameObject.name))
+            {
+                inventory.Add(rayHit.collider.gameObject.name);
+            }
+        }
+
+        if (rayHit.collider.gameObject.name == "Cabinet")
+        {
+            dialogueBox.SetActive(true);
+            text.SetActive(true);
+            closeButton.SetActive(true);
+            Text.text = "Huh, Lulieta's favorite mug is missing.";
+
+            if (!inventory.Contains(rayHit.collider.gameObject.name))
+            {
+                inventory.Add(rayHit.collider.gameObject.name);
+            }
+        }
+
+        if(rayHit.collider.gameObject.name == "Knife Holder")
+        {
+            dialogueBox.SetActive(true);
+            text.SetActive(true);
+            closeButton.SetActive(true);
+            Text.text = "One of the knives is missing.";
+
+            if (!inventory.Contains(rayHit.collider.gameObject.name))
+            {
+                inventory.Add(rayHit.collider.gameObject.name);
+            }
         }
 
         if (rayHit.collider.gameObject.name == "exit")
         {
-            SceneManager.LoadScene("Room 2");
+            Debug.Log("bbb");
+            SceneManager.LoadScene("roomDoors");
         }
 
         if (rayHit.collider.gameObject.name == "Lulieta")
         {
             SceneManager.LoadScene("Lulieta");
         }
+
+        if (rayHit.collider.gameObject.name == "door1")
+        {
+            SceneManager.LoadScene("Room 1");
+        }
+        if (rayHit.collider.gameObject.name == "door2")
+        {
+            if(inventory.Contains("knife") && inventory.Contains("mug") && inventory.Contains("rope"))
+            {
+                Debug.Log("lol");
+                SceneManager.LoadScene("Room 2");
+            }
+        }
+        if (rayHit.collider.gameObject.name == "door3")
+        {
+            //SceneManager.LoadScene("Room 1");
+        }
     }
 
     IEnumerator Typing()
     {
-        foreach(char letter in dialogue[index].ToCharArray())
+        
+        foreach (char letter in dialogue[index].ToCharArray())
         {
             Text.text += letter;
             yield return new WaitForSeconds(wordspeed);
         }
+        
     }
-
+    
     public void NextLine()
     {
         continueButton.SetActive(false);
@@ -123,6 +219,7 @@ public class InputHandler : MonoBehaviour
             dialogueBox.SetActive(false);
             text.SetActive(false);
         }
+        
     }
 
     public void Back()
